1ba1da1fdde1a0b66220a1124bf7d75d29aba73f
maanasanarayan <84228304+maanasanarayan@users.noreply.github.com>
2022-01-04 18:50:03 -0500

    Time: 0 ms (100.00%), Space: 37.5 MB (20.83%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int[] stair = new int[n + 1];
+        stair[0] = 1;
+        stair[1] = 2;
+        
+        for(int i = 2; i < n; i++) {
+            stair[i] = stair[i-1] + stair[i-2];
+        }
+        
+        return stair[n - 1];
+    }
+}
