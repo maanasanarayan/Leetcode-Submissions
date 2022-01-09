5caac09aca129beee1c0822dc50372e4495bd105
Maanasa Narayan <84228304+maanasanarayan@users.noreply.github.com>
2022-01-08 22:58:11 -0500

    Time: 12 ms (98.64%), Space: 35.4 MB (95.89%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,6 +3,8 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
+        
+        /*
         int lp = 1;
         int rp = n;
         
@@ -18,6 +20,21 @@ public class Solution extends VersionControl {
                 lp = mid + 1;
             }
         }
-        return -1;        
+        return -1;           
+        */
+        
+        int lp = 1;
+        int rp = n;
+        
+        while(lp < rp) {
+            int mid = lp + (rp - lp) / 2;
+            
+            if(isBadVersion(mid)) {
+                rp = mid;
+            } else {
+                lp = mid + 1;
+            }
+        }
+        return lp;   
     }
 }
