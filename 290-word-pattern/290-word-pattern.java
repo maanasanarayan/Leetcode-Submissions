6d5360e3c8f75756c01037fba50920aadc266515
Maanasa Narayan <84228304+maanasanarayan@users.noreply.github.com>
2022-01-17 23:20:22 -0500

    Time: 2 ms (30.68%), Space: 38.8 MB (21.01%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        Map<Character,String> map = new HashMap<>();
+            
+        if(pattern.length() != words.length) {
+            return false;
+        }
+        
+        for(int i = 0; i < pattern.length(); i++) {
+            Character ch = new Character(pattern.charAt(i));
+            if(map.containsKey(ch)) {
+                if(! map.get(ch).equals(words[i])) {
+                    return false;
+                } 
+            } else {
+                for(Character st : map.keySet()) {
+                    if(map.get(st).equals(words[i])) {
+                        return false;
+                    }
+                }
+                map.put(ch, words[i]);
+            }
+        }
+        return true;
+    }
+}
