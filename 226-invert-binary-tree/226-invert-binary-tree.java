fada7d682e5dbce82815a952286af1fcc618f7d2
Maanasa Narayan <84228304+maanasanarayan@users.noreply.github.com>
2022-03-19 00:08:21 -0400

    Time: 0 ms (100.00%), Space: 41.4 MB (57.27%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -15,7 +15,8 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        // Recursion
+        // Recursive Solution
+        /*
         if(root == null) {
             return null;
         }
@@ -25,6 +26,30 @@ class Solution {
         root.left = right;
         root.right = left;
         
+        return root;
+        */
+        
+        //Iterative Solution
+        if(root == null) {
+            return null;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+            
+            if(current.left != null) {
+                queue.add(current.left);
+            }
+            if(current.right != null) {
+                queue.add(current.right);
+            }
+        }
         return root;
     }
 }
