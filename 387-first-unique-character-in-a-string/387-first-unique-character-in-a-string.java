714147292299f9b49088444a0efb45adaa283b01
Maanasa Narayan <84228304+maanasanarayan@users.noreply.github.com>
2022-03-24 12:37:01 -0400

    Time: 75 ms (7.44%), Space: 54.5 MB (17.87%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,5 +1,8 @@
 class Solution {
     public int firstUniqChar(String s) {
+        
+        // Both map and set (My solution) - map for storing all unique characters and its index, set for storing the repeated characters.
+        /*
         Map<Character, Integer> map = new HashMap<>();
         Set<Character> repeated = new HashSet<>();
         
@@ -26,5 +29,24 @@ class Solution {
         }
         
         return min;
+        */
+        //Only map, going over the input array twice
+        
+        Map<Character, Integer> count = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(count.containsKey(s.charAt(i))) {
+                count.put(s.charAt(i), count.get(s.charAt(i)) + 1);
+            } else {
+                count.put(s.charAt(i), 1);
+            }
+        }
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(count.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
