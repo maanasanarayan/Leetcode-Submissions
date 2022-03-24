40a720f02435650ae5183ae77e2a578b0cd3341b
Maanasa Narayan <84228304+maanasanarayan@users.noreply.github.com>
2022-03-23 23:17:38 -0400

    Time: 20 ms (30.88%), Space: 46.2 MB (83.79%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,5 +1,8 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        
+        //Sorted keys
+        /*
         List<List<String>> result = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         
@@ -12,6 +15,31 @@ class Solution {
                 map.get(key).add(strs[i]);
             else 
                 map.put(key, new ArrayList<>(Arrays.asList(strs[i])));
+        }
+        
+        for(List<String> value : map.values()) {
+            result.add(value);
+        }
+        
+        return result;
+        */
+        
+        //Character count
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for(int i = 0; i < strs.length; i++) {
+            int[] cnt = new int[26];
+            for(int j = 0; j < strs[i].length(); j++) {
+                cnt[strs[i].charAt(j) - 97]++;
+            }
+            String key = Arrays.toString(cnt);
+            
+            if(map.containsKey(key)) {
+                map.get(key).add(strs[i]);
+            } else {
+                map.put(key, new ArrayList<>(Arrays.asList(strs[i])));
+            }
         }
         
         for(List<String> value : map.values()) {
