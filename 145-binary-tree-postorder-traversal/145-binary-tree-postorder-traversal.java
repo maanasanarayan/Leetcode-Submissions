9e93cea479bc826d773dc428bf29adab5e042811
Maanasa Narayan <84228304+maanasanarayan@users.noreply.github.com>
2022-03-31 10:15:01 -0400

    Time: 1 ms (44.87%), Space: 42.2 MB (50.56%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -15,21 +15,44 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        if(root != null) {
-            return postOrder(root, new ArrayList<>());
-        } 
-        return new ArrayList<>();        
+        // if(root != null) {
+        //     return postOrder(root, new ArrayList<>());
+        // } 
+        // return new ArrayList<>();     
+        
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> st = new Stack();
+        
+        while(!st.isEmpty() || root != null) {
+            while(root != null) {
+                st.push(root);
+                root = root.left;
+            }
+            
+            while(!st.isEmpty() && root == st.peek().right) {
+                root = st.pop();
+                res.add(root.val);
+            }
+            
+            if(!st.isEmpty() && root != st.peek().right) {
+                root = st.peek().right;
+            }
+            
+            if(st.isEmpty()) break;
+        }
+        
+        return res;
     }
     
-    private List<Integer> postOrder(TreeNode root, List<Integer> lst) {
-        if(root.left != null) {
-            postOrder(root.left, lst);
-        }
-        if(root.right != null) {
-            postOrder(root.right, lst);
-        }
-        lst.add(root.val);
+//     private List<Integer> postOrder(TreeNode root, List<Integer> lst) {
+//         if(root.left != null) {
+//             postOrder(root.left, lst);
+//         }
+//         if(root.right != null) {
+//             postOrder(root.right, lst);
+//         }
+//         lst.add(root.val);
         
-        return lst;
-    }
+//         return lst;
+//     }
 }
